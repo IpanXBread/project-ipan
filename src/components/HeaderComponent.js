46c2ca4b94e36894eb801ff71fefb8e6eefd51e1
IpanXBread <ipanxbread@gmail.com>
2024-01-22 11:51:02 +0800

    mobile view - drop down menu

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import HeaderContent from "./HeaderContent";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDev } from '@fortawesome/free-brands-svg-icons';
-import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import { faToggleOn, faToggleOff, faBars } from '@fortawesome/free-solid-svg-icons';
 import '../css/Header.css';
 import '../css/styles.css';
 
 export default function HeaderComponent() {
     const storedMode = localStorage.getItem('themeMode');
+    const storedMobileView = localStorage.getItem('mobileView');
+    
     const [isLightMode, setIsLightMode] = useState(storedMode === 'light');
+    const [isMobile, setIsMobile] = useState(
+        storedMobileView === null ? window.innerWidth < 600 : JSON.parse(storedMobileView)
+    );
+    const [isDropMenuOpen, setIsDropMenuOpen] = useState(false);
 
+     // Handle Light/Dark mode
+     useEffect(() => {
+        document.body.classList.toggle('light-mode', isLightMode);
+    }, [isLightMode]);
+
+    // Handle Mobile/Web view
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 600);
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    
     const toggleMode = () => {
         const newMode = !isLightMode ? 'light' : 'dark';
         setIsLightMode((prevMode) => !prevMode);
@@ -17,26 +39,53 @@ export default function HeaderComponent() {
         localStorage.setItem('themeMode', newMode);
     };
 
-    useEffect(() => {
-        document.body.classList.toggle('light-mode', isLightMode);
-    }, [isLightMode]);
+    const toggleDropMenu = () => {
+        setIsDropMenuOpen((prevState) => !prevState);
+    }
 
     return (
-        <div className="space-between">
-            <div className="header-container center-horizontal center-vertical">
-                <HeaderContent headerName="HOME" destinationURL="/home" />
-                <HeaderContent headerName="WORK" destinationURL="/work" />
-                <HeaderContent headerName="PROJECT" destinationURL="/project" />
-                <HeaderContent headerName="SKILL" destinationURL="/skills" />
-            </div>
-            <div className="center-horizontal center-vertical icon">
-                <FontAwesomeIcon
-                    icon={isLightMode ? faToggleOn : faToggleOff}
-                    className="icon close-button"
-                    id="toggleButton"
-                    onClick={toggleMode}
-                />
-            </div>
-        </div>
+        <>
+            {isMobile ? (
+                <>
+                    <div>
+                        <div className="header-container" style={{ justifyContent: "end" }}>
+                            <div style={{ width: "30px", height: "30px", display: "flex", justifyContent:"center", alignItems:"center", marginTop:"-20px", marginRight:"-15px", zIndex:"999"}} onClick={toggleDropMenu}>
+                                <FontAwesomeIcon
+                                    icon={faBars}
+                                    className="icon close-button"
+                                    id="toggleButton"
+                                />
+                            </div>
+                            {isDropMenuOpen && (
+                                <div className="mobile-menu">
+                                    <HeaderContent headerName="HOME" destinationURL="/home"/>
+                                    <HeaderContent headerName="WORK" destinationURL="/work" />
+                                    <HeaderContent headerName="PROJECT" destinationURL="/project" />
+                                    <HeaderContent headerName="SKILL" destinationURL="/skills" />
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div className="space-between">
+                    <div className="header-container center-horizontal center-vertical">
+                        <HeaderContent headerName="HOME" destinationURL="/home" />
+                        <HeaderContent headerName="WORK" destinationURL="/work" />
+                        <HeaderContent headerName="PROJECT" destinationURL="/project" />
+                        <HeaderContent headerName="SKILL" destinationURL="/skills" />
+                    </div>
+                    <div className="center-horizontal center-vertical icon">
+                        <FontAwesomeIcon
+                            icon={isLightMode ? faToggleOn : faToggleOff}
+                            className="icon close-button"
+                            style={{ marginTop: "-10px" }}
+                            id="toggleButton"
+                            onClick={toggleMode}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../css/styles.css';
 import '../css/Header.css';
 
 export default function HeaderContent(props) {
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
   const isActive = location.pathname === props.destinationURL;
 
   const style = {
     ...headerContentStyle,
     color: isActive ? 'var(--primary-color)' : 'var(--text-color)',
     fontFamily: 'var(--josefin-sans)',
+    padding: "15px 0px 15px 0px",
   };
 
   return (
-    <a href={props.destinationURL} style={style} className="text-reset">
-      <p className="josefin-sans header-text">{props.headerName}</p>
-    </a>
+    <>
+      {isMobile ? (
+        <a href={props.destinationURL} style={style} className="text-reset horizontal">
+          <p className="josefin-sans" style={{marginTop:"10px", marginRight: "10px"}}>✦</p>
+          <p className="josefin-sans">{props.headerName}</p>
+        </a>
+      ) : (
+        <a href={props.destinationURL} style={style} className="text-reset">
+          <p className="josefin-sans header-text">{props.headerName}</p>
+        </a>
+      )}
+    </>
   );
 }
 
 const headerContentStyle = {
-  margin: '-25px 25px',
+  margin: '-25px 15px',
 };
